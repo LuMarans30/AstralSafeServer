861a876222449b2a3588d8460fdce54720c7de22
LuMarans30 <andreamarano2004@gmail.com>
2023-01-28 19:57:30 +0100

    new folder structure (css and js)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('./public'));
+app.use(express.static('./'));
 
 
 const PORT = process.env.PORT || 8080;
